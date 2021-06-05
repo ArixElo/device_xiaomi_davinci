51295a668b511877f30f95e32d761c761699a496
ArixElo <patroxgamer@gmail.com>
2021-06-05 15:59:12 +0000

    davinci: remove unnecessary dependencies and add vendorsetup script
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+### Removing and cloning good HALs ###
+rm -rf hardware/qcom-caf/sm8150/audio && git clone https://github.com/Laker87/android_hardware_qcom_audio.git -b 11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media && git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/media
+
+### Restoring SSOS CAF Telephony repo ###
+repo sync --force-sync android_vendor_codeaurora_telephony
