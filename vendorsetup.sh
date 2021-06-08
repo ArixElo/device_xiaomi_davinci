514a7efec29b63bf6472516f926f2988fc627303
ArixElo <patroxgamer@gmail.com>
2021-06-08 19:37:38 +0200

    davinci: vendorsetup: Add more repos to be force-synced
    They are gonna replace the Telephony stuff required for mido
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,7 @@ rm -rf hardware/qcom-caf/sm8150/audio && git clone https://github.com/Laker87/an
 rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/media && git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/media
 
-### Restoring SSOS CAF Telephony repo ###
+### Restoring SSOS CAF Telephony repos ###
 repo sync --force-sync android_vendor_codeaurora_telephony
+repo sync --force-sync android_frameworks_opt_telephony
+repo sync --force-sync android_packages_services_Telephony
